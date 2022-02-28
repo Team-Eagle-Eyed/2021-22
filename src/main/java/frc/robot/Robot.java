a2d6528d6832b2ee43f0a1ad5d029ad1fbd80d09
John Gooden <johnnyg3587@gmail.com>
2022-02-28 08:36:07 -0600

    shuffleboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,6 +15,8 @@ import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class Robot extends TimedRobot {
   //variables galore
   private DifferentialDrive m_myRobot;
@@ -47,11 +49,11 @@ public class Robot extends TimedRobot {
     m_intakeArm = new PWMSparkMax(8); //pwm cause mentor asked for it
     m_intake = new PWMSparkMax(9); //pwm cause mentor asked for it
 
-    m_rightArm.setOpenLoopRampRate(0.1);
+    m_rightArm.setOpenLoopRampRate(0.1); //ramp rate so the motor cant stall when jumping to full power
     m_leftArm.setOpenLoopRampRate(0.1);
     m_rightMotor.setOpenLoopRampRate(0.1);
     m_leftMotor.setOpenLoopRampRate(0.1);
-    m_rightArm.setIdleMode(IdleMode.kBrake);
+    m_rightArm.setIdleMode(IdleMode.kBrake); //brake so the arms don't coast past the limit switches
     m_leftArm.setIdleMode(IdleMode.kBrake);
 
     m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
@@ -63,15 +65,38 @@ public class Robot extends TimedRobot {
     m_rightMotor.setInverted(true);
 
     CameraServer.startAutomaticCapture(); //woo I have eyes
+
+    SmartDashboard.putBoolean("driveEnabled", true);
+    SmartDashboard.putBoolean("armsEnabled", true);
+    SmartDashboard.putBoolean("intakeEnabled", true);
+    SmartDashboard.putNumber("m_leftArm", 0);
+    SmartDashboard.putNumber("m_rightArm", 0);
+    SmartDashboard.putNumber("m_leftMotor", 0);
+    SmartDashboard.putNumber("m_rightMotor", 0);
+    SmartDashboard.putNumber("m_intakeArm", 0);
+    SmartDashboard.putNumber("m_intake", 0);
+    SmartDashboard.putNumber("speedController", 0);
   }
 
   @Override
   public void teleopPeriodic() {
+    SmartDashboard.putNumber("m_leftArm", m_leftArm.get());
+    SmartDashboard.putNumber("m_rightArm", m_rightArm.get());
+    SmartDashboard.putNumber("m_leftMotor", m_leftMotor.get());
+    SmartDashboard.putNumber("m_rightMotor", m_rightMotor.get());
+    SmartDashboard.putNumber("m_intakeArm", m_intakeArm.get());
+    SmartDashboard.putNumber("m_intake", m_intake.get());
+
     double speedController = driveStick.getRawAxis(3); //little dial thing on the front of the controller
     double speed1 = speedController - 1; //maths
     double speed = speed1 / 2; //more maths
+    SmartDashboard.putNumber("speedController", speed * -1);
 
-    m_myRobot.arcadeDrive(driveStick.getY() * speed, -driveStick.getX() * speed); //vroom vroom
+    if(SmartDashboard.getBoolean("driveEnabled", true) == true) {
+      m_myRobot.arcadeDrive(driveStick.getY() * speed, -driveStick.getX() * speed); //vroom vroom
+    } else {
+      m_myRobot.arcadeDrive(0, 0);
+    }
 
     double leftArmSpeed = armGamepad.getRawAxis(1); //y axis for left stick
     double rightArmSpeed = armGamepad.getRawAxis(5); //y axis for right stick
@@ -99,27 +124,46 @@ public class Robot extends TimedRobot {
       }
     }
 
-    m_leftArm.set(leftArmOut);
-    m_rightArm.set(rightArmOut);
+    if(SmartDashboard.getBoolean("armsEnabled", true) == true) {
+      m_leftArm.set(leftArmOut);
+      m_rightArm.set(rightArmOut);
+    } else {
+      m_leftArm.set(0);
+      m_rightArm.set(0);
+    }
 
     boolean armUp = armGamepad.getRawButton(4); //y button
     boolean armDown = armGamepad.getRawButton(1); //a button
     boolean intake = armGamepad.getRawButton(3); //x button
+    boolean intake2 = armGamepad.getRawButton(2);
+    double intakeOut = 0;
+    double intakeArmOut = 0;
 
     if(armUp) { //intake arm control
-      m_intakeArm.set(0.5); //half speed cause I don't know how fast it will be yet
+      intakeArmOut = 0.5; //half speed cause I don't know how fast it will be yet
     } else if(armDown) {
-      m_intakeArm.set(-0.5);
+      intakeArmOut = -0.5;
+    } else {
+      intakeArmOut = 0;
+    }
+    if(SmartDashboard.getBoolean("intakeEnabled", true) == true) { //if intake enabled, set intake arm speed
+      m_intakeArm.set(intakeArmOut);
     } else {
       m_intakeArm.set(0);
     }
-
+    
     if(intake) { //intake control
-      m_intake.set(0.5); //also half speed, subject to change, same reason as above
+      intakeOut = 0.5;
+    } else if(intake2) {
+      intakeOut = -0.5;
+    } else {
+      intakeOut = 0;
+    }
+    if(SmartDashboard.getBoolean("intakeEnabled", true) == true) { //if intake enabled, set intake speed
+      m_intake.set(intakeOut);
     } else {
       m_intake.set(0);
     }
-
   }
 
   @Override
