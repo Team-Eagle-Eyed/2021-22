815599da40f56818fa6961e5e4b9a6bb7d95bffe
jasondaming <jasondaming@gmail.com>
2022-03-01 23:15:06 -0600

    suggested changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,15 +4,15 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-import com.fasterxml.jackson.core.io.OutputDecorator;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
@@ -20,7 +20,7 @@ public class Robot extends TimedRobot {
   private DifferentialDrive m_myRobot;
   
   private Joystick driveStick;
-  private Joystick armGamepad;
+  private XboxController armGamepad;
 
   private static final int leftDriveID = 1; 
   private static final int rightDriveID = 2;
@@ -38,15 +38,17 @@ public class Robot extends TimedRobot {
   double leftArmOut = 0;
   double rightArmOut = 0;
 
-  double autoStartTime = 0;
+  Timer teleopTimer;
+  Timer autoTimer;
   boolean runAuto = true;
-
-  double teleopStartTime = 0;
+  Double gamepadDeadband = .05; // I like to declare inline, especially for simple things
+  Double armFeedForward = .15;
 
   @Override
   public void robotInit() {
-    m_leftMotor = new CANSparkMax(leftDriveID, MotorType.kBrushless); //woo canbus stuff
+    m_leftMotor = new CANSparkMax(leftDriveID, MotorType.kBrushless);
     m_rightMotor = new CANSparkMax(rightDriveID, MotorType.kBrushless);
+    
     m_leftArm = new CANSparkMax(leftArmID, MotorType.kBrushless);
     m_rightArm = new CANSparkMax(rightArmID, MotorType.kBrushless);
     m_intakeArm = new PWMSparkMax(8); //pwm cause mentor asked for it
@@ -60,9 +62,11 @@ public class Robot extends TimedRobot {
     m_leftArm.setIdleMode(IdleMode.kBrake);
 
     m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
+    m_myRobot.setDeadband(0.05);
 
     driveStick = new Joystick(0); //big joystick thing
-    armGamepad = new Joystick(1); //small standard console controller
+    armGamepad = new XboxController(1); //small standard console controller
+    
     m_leftArm.setInverted(true); // backwards, to add more confusion
     m_leftMotor.setInverted(false);
     m_rightMotor.setInverted(true);
@@ -88,7 +92,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    teleopStartTime = Timer.getFPGATimestamp();
+    teleopTimer = new Timer();
+    teleopTimer.start();
   }
 
   @Override
@@ -100,24 +105,22 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("m_intakeArm", m_intakeArm.get());
     SmartDashboard.putNumber("m_intake", m_intake.get());
 
-    double teleopTimeElapsed = Timer.getFPGATimestamp() - teleopStartTime;
-
     double speedController = driveStick.getRawAxis(3); //little "dial" thing on the front of the controller
     double speed1 = speedController - 1; //maths
     double speed = speed1 / 2; //more maths
     SmartDashboard.putNumber("speedController", speed * -1);
 
     if(SmartDashboard.getBoolean("driveEnabled", true) == true) {
-      m_myRobot.arcadeDrive(driveStick.getY() * speed, -driveStick.getX() * speed); //vroom vroom
+      m_myRobot.arcadeDrive(driveStick.getY() * speed, -driveStick.getZ() * speed, true); // Use twist for turning.  Square inputs for better control at low speed.
     } else {
       m_myRobot.arcadeDrive(0, 0);
     }
 
-    double leftArmSpeed = armGamepad.getRawAxis(1); //y axis for left stick
-    double rightArmSpeed = armGamepad.getRawAxis(5); //y axis for right stick
+    double leftArmSpeed = MathUtil.applyDeadband(armGamepad.getLeftY(), gamepadDeadband);
+    double rightArmSpeed = MathUtil.applyDeadband(armGamepad.getRightY(), gamepadDeadband);
 
-    boolean armRB = armGamepad.getRawButton(6); //button 6 for right bumper
-    boolean armLB = armGamepad.getRawButton(5); //button 5 for left bumper
+    boolean armRB = armGamepad.getRightBumper(); //button 6 for right bumper
+    boolean armLB = armGamepad.getLeftBumper(); //button 5 for left bumper
 
     if(armRB == true) { //retracts right arm, extends left. Note left arm is inverted.
       leftArmOut = -1;
@@ -125,18 +128,10 @@ public class Robot extends TimedRobot {
     } else if(armLB == true) { //retracts left arm, extends right. Note left arm is inverted.
       leftArmOut = 1;
       rightArmOut = -1;
-    } else if(armRB == false && armLB == false) { //if neither bumber is pressed, joysticks take over
-      if(Math.abs(leftArmSpeed) > 0.05) { //left arm deadband
-        leftArmOut = leftArmSpeed;
-      } else {
-        leftArmOut = 0;//who is insane enough to put a comment without a space separating it from the code?
-      }
-  
-      if(Math.abs(rightArmSpeed) > 0.05) { //right arm deadband
-        rightArmOut = rightArmSpeed;
-      } else {
-        rightArmOut = 0;
-      }
+    } else { //if neither bumber is pressed, joysticks take over, we know they aren't pressed because above
+        leftArmOut = Math.copySign(Math.abs(leftArmSpeed * (1-armFeedForward)) + armFeedForward, leftArmSpeed); // controller output is % pressed after deadband
+        rightArmOut = Math.copySign(Math.abs(rightArmSpeed * (1-armFeedForward)) + armFeedForward, rightArmSpeed); // We multiply by the remaining range and then add the feedforward
+        // the copySign stuff is just to maintain the -/+ when adding a scalar
     }
 
     if(SmartDashboard.getBoolean("armsEnabled", true) == true) {
@@ -147,10 +142,10 @@ public class Robot extends TimedRobot {
       m_rightArm.set(0);
     }
 
-    boolean armUp = armGamepad.getRawButton(4); //y button
-    boolean armDown = armGamepad.getRawButton(1); //a button
-    boolean intake = armGamepad.getRawButton(3); //x button
-    boolean intake2 = armGamepad.getRawButton(2);
+    boolean armUp = armGamepad.getYButton();
+    boolean armDown = armGamepad.getAButton();
+    boolean intake = armGamepad.getXButton();
+    boolean intake2 = armGamepad.getBButton();
     double intakeOut = 0;
     double intakeArmOut = 0;
 
@@ -183,7 +178,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    autoStartTime = Timer.getFPGATimestamp();
+    autoTimer = new Timer();
+    autoTimer.start();
   }
 
 
@@ -197,7 +193,7 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("m_intake", m_intake.get());
     
     //get a time for autonomous start to do events based on time later
-    double autoTimeElapsed = Timer.getFPGATimestamp() - autoStartTime; //get time since start of auto
+    double autoTimeElapsed = autoTimer.get(); //get time since start of auto
     if(autoTimeElapsed < 2.5) { //move for 3 seconds
       m_leftMotor.set(0.25);
       m_rightMotor.set(0.25);
